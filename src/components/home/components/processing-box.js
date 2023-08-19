de96fb7507a21ee0241d0ea29a13ef5485ca0b04
Tariqali <tariq@fullstacklabs.co>
2023-08-19 13:50:21 +0500

    fix css

--- src/components/home/components/processing-box.js
+++ src/components/home/components/processing-box.js
@@ -8,12 +8,12 @@ const ProcessingBox = (props) => {
     handleCancel,
   } = props;
   return (
-    <div className="w-full relative flex flex-col justify-center items-center px-[135px] pt-[58.63px] pb-[32.39px]">
+    <div className="w-full relative flex flex-col justify-center items-center pt-[58.63px] pb-[32.39px]">
       <div class="chase-spinner">
         <div class="dot1"></div>
         <div class="dot2"></div>
       </div>
-      <div className="font-dmsans text-stone-100 mt-[29px] mb-2 text-[22px] w-full font-normal leading-[33px]">
+      <div className="font-dmsans flex flex-col justify-center items-center text-stone-100 mt-[29px] mb-2 text-[22px] w-full font-normal leading-[33px]">
         {isLoadingUploading && "(1/3) uploading..."}
         {isLoadingSpeachToText && "(2/3) transcribing..."}
         {isLoadingSummarizeText && "(3/3) rewriting..."}
